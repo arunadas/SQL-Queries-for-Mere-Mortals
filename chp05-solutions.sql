a8d6810f7190876f4d14621e4f6592aff26da6f1
arunadas <aruna.das29@gmail.com>
2024-07-07 23:15:14 -0700

    solution to chapter 5

--- chp05-solutions.sql
+++ chp05-solutions.sql
@@ -3,3 +3,9 @@ set search_path to salesordersexample;
 /* What if we adjusted each product price by reducing it 5 percent */
 select retailprice as orginalPrice, 
        round((retailprice - (5/100.0)*retailprice)) as adjustedPrice from products;
+
+/*Show me a list of orders made by each customer in descending date order*/  
+select customerid, ordernumber, orderdate 
+from orders
+order by 
+customerid, orderdate desc     
