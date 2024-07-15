94b8cb3c519e815adbaf100a9ca8af6e766837b8
arunadas <aruna.das29@gmail.com>
2024-07-14 21:24:12 -0700

    solution to chapter 6

--- chp06-solution.sql
+++ chp06-solution.sql
@@ -4,3 +4,8 @@ set search_path to salesordersexample;
 select vendname from vendors
 where vendcity IN ( 'Bellevue', 'Ballard', 
 				   'Redmond')
+
+/*Q2 Show me an alphabetize list of products with a retail price of $125.00 or more */
+select productname, retailprice from products
+where retailprice >= 125
+order by productname                   
