9b24c663f71a64b7ea76a7fde6437d83f695a5c8
arunadas <aruna.das29@gmail.com>
2024-08-03 21:07:28 -0700

    solution to chapter 8

--- chpo8-solutions.sql
+++ chpo8-solutions.sql
@@ -92,7 +92,7 @@ on scs.classstatus = ss.classstatus
 where scs.classstatusdescription = 'Enrolled'
 order by 1
 
-/** Q12 List the faculty staff and the subject each teaches */
+/* Q12 List the faculty staff and the subject each teaches */
 select distinct concat(stffirstname, ' ', stflastname) as stfName , sb.subjectname
 from  staff s
 inner join faculty_subjects fs 
