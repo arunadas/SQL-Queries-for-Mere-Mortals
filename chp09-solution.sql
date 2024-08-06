e8ad5b7484f3d3b310b7981f1b6391f5da40efb1
arunadas <aruna.das29@gmail.com>
2024-08-05 21:07:19 -0700

    solution to chapter 9

--- chp09-solution.sql
+++ chp09-solution.sql
@@ -11,3 +11,9 @@ inner join products p on od.productnumber = p.productnumber
 where p.productname like '%Helmet%' ) helmet 
 on c.customerid = helmet.customerid
 where helmet.customerid is null
+
+/*Q2 Display customers who have no sales rep (employees) in the same zip code */
+select concat(c.custfirstname,' ', c.custlastname) as customerName
+from customers c 
+left join employees e on c.custzipcode = e.empzipcode
+where e.employeeid is null
