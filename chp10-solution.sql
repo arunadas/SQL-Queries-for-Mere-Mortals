d583427e31ebb3462bc2003273423888f20bd60c
arunadas <aruna.das29@gmail.com>
2024-08-21 20:47:00 -0700

    solution to chapter 10

---
+++ chp10-solution.sql
@@ -0,0 +1,22 @@
+set search_path to salesordersexample;
+
+/*Q1 list the customers who ordered a helmet together with the vendors who provide helmets */
+
+/*Q1 list the customers who ordered a helmet together with the vendors who provide helmets */
+select  concat(custfirstname,' ', custlastname) as customers , p.productname, 'customer' as rowid from customers c
+inner join orders o  
+on c.customerid = o.customerid 
+inner join order_details od 
+on o.ordernumber = od.ordernumber
+inner join products p 
+on p.productnumber = od.productnumber
+where p.productname like '%Helmet%'
+
+union  
+
+select  vendname, p.productname, 'vendor' as rowid from vendors v
+inner join product_vendors pv  
+on v.vendorid = pv.vendorid
+inner join products p 
+on p.productnumber = pv.productnumber
+where p.productname like '%Helmet%'
