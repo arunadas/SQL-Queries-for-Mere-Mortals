6d98873c760ffc255e5cde578e58405d87bdc0e0
arunadas <aruna.das29@gmail.com>
2024-10-24 22:55:20 -0700

    solution to chapter 11

--- chp11-solution.sql
+++ chp11-solution.sql
@@ -88,6 +88,6 @@ SET SEARCH_PATH TO schoolschedulingexample;
 /* List all staff members and the count of classes each teaches */
 
 select concat(stffirstname , ' ' , stflastname) as staffName , 
-(select count(f.classid) from faculty_classes f 
+(select count(f.classid) AS classCount from faculty_classes f 
 where f.staffid = s.staffid)
 from staff s
